465f3ca102596978f186439175256daaedeb7633
weixin_43889252 <bonbonx01@163.com>
2025-02-17 16:15:04 +0800

    feat:通用群聊消息处理器

--- im-server-application/src/main/java/io/txuan/im/server/application/netty/processor/factory/ProcessorFactory.java
+++ im-server-application/src/main/java/io/txuan/im/server/application/netty/processor/factory/ProcessorFactory.java
@@ -2,6 +2,7 @@ package io.txuan.im.server.application.netty.processor.factory;
 
 import io.txuan.im.common.domain.enums.IMCmdType;
 import io.txuan.im.server.application.netty.processor.MessageProcessor;
+import io.txuan.im.server.application.netty.processor.impl.GroupMessageProcessor;
 import io.txuan.im.server.application.netty.processor.impl.LoginProcessor;
 import io.txuan.im.server.application.netty.processor.impl.PrivateMessageProcessor;
 import io.txuan.im.server.infrastructure.holder.SpringContextHolder;
@@ -18,8 +19,11 @@ public class ProcessorFactory {
                 return SpringContextHolder.getApplicationContext().getBean(HeartbeatProcessor.class);
                 //单聊消息
             case PRIVATE_MESSAGE:
-                return SpringContextHolder.getApplicationContext().getBean(PrivateMessageProcessor.class);                //群聊消息
+                return SpringContextHolder.getApplicationContext().getBean(PrivateMessageProcessor.class);
+                //群聊消息
             case GROUP_MESSAGE:
+                return SpringContextHolder.getApplicationContext().getBean(GroupMessageProcessor.class);
+
             default:
                 return null;
 
